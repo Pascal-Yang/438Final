c81af4247cca35566c6191a42a76d21a23908d30
Pascal-Yang <pascal@wustl.edu>
2022-10-22 19:53:31 -0500

    hello from pascal

---
+++ Scribbly/Scribbly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scribbly
+//
+//  Created by Pascal Yang on 10/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
